811d6c8f3f8de73abcf18e2261651462892bd180
Daniel Thirion <defvs.daniel@gmail.com>
2022-12-01 17:15:16 +0100

    Day 1 of AOC

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,19 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+	// Returns a List of Ints containing the sum of calories for each elf.
+	fun getAllElvesCalories(input: List<String>) = input.joinToString(separator = "\n")
+		.split("\n\n").map { it.split("\n").sumOf { it.toInt() } }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+	// Get the maximum value of getAllElvesCalories
+	fun part1(input: List<String>) = getAllElvesCalories(input).max()
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+	// Sum the 3 maximum values of getAllElvesCalories
+	fun part2(input: List<String>) = getAllElvesCalories(input).sortedDescending().take(3).sum()
 
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+	// test if implementation meets criteria from the description, like:
+	val testInput = readInput("Day01_test")
+	check(part1(testInput).also { println("Test result was: $it") } == 24000)
+
+	val input = readInput("Day01")
+	println(part1(input))
+	println(part2(input))
 }
--- src/Utils.kt
+++ src/Utils.kt
@@ -1,3 +1,5 @@
+@file:Suppress("unused")
+
 import java.io.File
 import java.math.BigInteger
 import java.security.MessageDigest
@@ -6,11 +8,11 @@ import java.security.MessageDigest
  * Reads lines from the given input txt file.
  */
 fun readInput(name: String) = File("src", "$name.txt")
-    .readLines()
+	.readLines()
 
 /**
  * Converts string to md5 hash.
  */
 fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
-    .toString(16)
-    .padStart(32, '0')
+	.toString(16)
+	.padStart(32, '0')
